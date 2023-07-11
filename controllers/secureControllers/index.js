57b3195796b204ea7f3bdbdc8320cf9d726182e5
rakibul2180@gmail.com <rakibul2180@gmail.com>
2023-07-12 00:29:56 +0600

    transaction and my courses api added

--- controllers/secureControllers/index.js
+++ controllers/secureControllers/index.js
@@ -1,3 +1,6 @@
+const MyCourse = require("../../models/myCourseModel");
+const Transaction = require("../../models/transactionModel");
+
 const stripe = require("stripe")(
   "sk_test_51L43RjDIsxhoLpzhcuQQuFKF3oJ4gkZGgMlVPwjQvfWKeorzEza7MKZFGCqVLSWNoH5q6MMDkble6nVheJk59zEb00PSqvHAjZ"
 );
@@ -27,6 +30,40 @@ const paymentIntent = async (req, res) => {
   }
 };
 
+const createTransaction = async (req, res) => {
+  try {
+    const data = req.body;
+    const transaction = await Transaction.create({ ...data });
+    if (transaction) {
+      res.status(200).json(transaction);
+    } else {
+      res.status(400).json({ message: "Something wrong" });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.toString(),
+    });
+  }
+};
+
+const createMyCourse = async (req, res) => {
+  try {
+    const data = req.body;
+    const newCourse = await MyCourse.create({ ...data });
+    if (newCourse) {
+      res.status(200).json(newCourse);
+    } else {
+      res.status(400).json({ message: "Something wrong" });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.toString(),
+    });
+  }
+};
+
 module.exports = {
   paymentIntent,
+  createTransaction,
+  createMyCourse,
 };
--- routes/secureRoutes/index.js
+++ routes/secureRoutes/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { verifyAuth, verifyUser } = require("../../middleware/authMiddleware");
-const { paymentIntent } = require("../../controllers/secureControllers");
+const {
+  paymentIntent,
+  createTransaction,
+  createMyCourse,
+} = require("../../controllers/secureControllers");
 const router = express.Router();
 
 const middleware = [verifyAuth, verifyUser];
 router.use(middleware);
 
 router.post("/payment-intent", paymentIntent);
+router.post("/transaction", createTransaction);
+router.post("/my-courses", createMyCourse);
 
 module.exports = router;
